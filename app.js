46011d2b967e5b4685434e11531ace79720c3458
Bojja Ganesh Kumar <bojjaganeshkumar1@gmail.com>
2023-12-01 14:55:00 +0000

    modified

--- app.js
+++ app.js
@@ -105,36 +105,36 @@ app.get("/players/:playerId/matches/", async (request, response) => {
 app.get("/matches/:matchId/players/", async (request, response) => {
   const { matchId } = request.params;
   const getMatchPlayersQuery = `
-    select player_details.player_id,
-    player_details.player_name
-    from player_details join player_match_score on
-    player_details.player_id=player_match_score.player_id
-    where player_match_score.match_id=${matchId};`;
+	    SELECT
+	      player_details.player_id AS playerId,
+	      player_details.player_name AS playerName
+	    FROM player_match_score NATURAL JOIN player_details
+        WHERE match_id=${matchId};`;
   const matchPlayers = await db.all(getMatchPlayersQuery);
-  const matchPlayersMod = matchPlayers.map((item) => {
-    return {
-      playerId: item.player_id,
-      playerName: item.player_name,
-    };
-  });
-  response.send(matchPlayersMod);
+  //   const matchPlayersMod = matchPlayers.map((item) => {
+  //     return {
+  //       playerId: item.player_id,
+  //       playerName: item.player_name,
+  //     };
+  //   });
+  response.send(matchPlayers);
 });
 
 //Statistics of score API
 app.get("/players/:playerId/playerScores/", async (request, response) => {
   const { playerId } = request.params;
-  const getStatisticsQuery = `
-    select player_details.player_id as playerId,
-    player_details.player_name as playerName,
-    sum(player_match_score.score) as totalScore,
-    sum(player_match_score.fours) as totalFours,
-    sum(player_match_score.sixes) as totalSixes
-    from
-    player_details join player_match_score on
-    player_details.player_id=player_match_score.player_id
-    group by player_details.player_id
-    having playerId=${playerId};`;
-  const statistics = await db.get(getStatisticsQuery);
+  const getPlayerScored = `
+    SELECT
+    player_details.player_id AS playerId,
+    player_details.player_name AS playerName,
+    SUM(player_match_score.score) AS totalScore,
+    SUM(fours) AS totalFours,
+    SUM(sixes) AS totalSixes FROM 
+    player_details INNER JOIN player_match_score ON
+    player_details.player_id = player_match_score.player_id
+    WHERE player_details.player_id = ${playerId};
+    `;
+  const statistics = await db.get(getPlayerScored);
   response.send(statistics);
 });
 
